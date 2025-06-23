ebaf74516ace5ed82cab4d7a148ce394389c1b9d
Héctor Emilio Velásquez Pérez <hectoremilio1000@gmail.com>
2025-06-23 02:33:48 -0600

    fix(terraform): unifica archivo de IP estática y la referencia en helm_release

--- ingress-ip.tf
+++ ingress-ip.tf
@@ -2,12 +2,12 @@
 # IP pública estática para el Ingress
 ########################################
 resource "azurerm_public_ip" "ingress_ip" {
-  name                = "pos-ingress-ip"
+  name                = "pos-ingress-ip"                 # ← sigue igual
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  allocation_method = "Static"
-  sku               = "Standard"
+  allocation_method   = "Static"                         # estática ✔
+  sku                 = "Standard"                       # Standard ✔
 
   tags = {
     Component   = "Ingress"
